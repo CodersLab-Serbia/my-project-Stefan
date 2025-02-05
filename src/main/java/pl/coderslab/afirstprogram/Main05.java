5d9a6a1fda5534be040719dad695b0443dc24b82
Stefan Bajic <shtefeksrb@icloud.com>
2025-02-05 21:00:53 +0100

    Delete src/main/java/pl/coderslab directory

--- src/main/java/pl/coderslab/afirstprogram/Main05.java
+++
@@ -1,9 +0,0 @@
-package pl.coderslab.afirstprogram;
-
-public class Main05 {
-    public static void main(String[] args) {
-        // Below line has a syntax error - missing semicolon
-        //		System.out.println("Hello world")
-    }
-}
-
