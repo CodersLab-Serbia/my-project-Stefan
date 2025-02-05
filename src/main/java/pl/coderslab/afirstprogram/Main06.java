fcaea4933711e34654fd6af2138bb39a91c6d482
Stefan Bajic <shtefeksrb@icloud.com>
2025-02-05 20:54:43 +0100

    Delete src directory

--- src/main/java/pl/coderslab/afirstprogram/Main06.java
+++
@@ -1,9 +0,0 @@
-package pl.coderslab.afirstprogram;
-
-public class Main06 {
-    public static void main(String[] args) {
-        // Below line has a typo in println
-        //		System.out.prinln("Hello world");
-    }
-}
-
