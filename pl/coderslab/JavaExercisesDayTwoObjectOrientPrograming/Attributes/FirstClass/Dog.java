cdcdb23f72fb86932a847f45104d66b1fb1fd16e
Stefan Bajic <shtefeksrb@icloud.com>
2025-02-16 22:58:17 +0100

    Add files via upload

---
+++ pl/coderslab/JavaExercisesDayTwoObjectOrientPrograming/Attributes/FirstClass/Dog.java
@@ -0,0 +1,6 @@
+package pl.coderslab.JavaExercisesDayTwoObjectOrientPrograming.Attributes.FirstClass;
+public class Dog {
+    private String breed;
+    private String name;
+    private int age;
+}
